3840c3bb40ab87c9fab5ed7f54e781a9ec102111
sammy <samircollege24@gmail.com>
2023-10-19 16:27:00 +0530

    Store Setup & Individual Product Screen (Store) Completed

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <div className="text-center text-xs text-black">
+          &copy; 2023 FakeStoreName, Inc. All Rights Reserved.
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
